2f35f74d6d543be22a674b4bee98f011c695a97e
Alpha Lencho <Alphalenchoo@gmail.com>
2024-12-18 23:33:57 +0300

    Update Handling_Mouse_Events.py

--- OpenCV/Projects/Basics/Handling_Mouse_Events.py
+++ OpenCV/Projects/Basics/Handling_Mouse_Events.py
@@ -17,3 +17,36 @@ while(1):
     if key == 27:
         break
 cv.destroyAllWindows()
+
+
+
+
+# mouse callback function
+drawing = True
+shape = 'r'
+def draw_circle(event, x, y, flags, param):
+    global x1, x2
+    if event == cv.EVENT_LBUTTONDOWN:
+        drawing = True
+        x1, x2 = x, y
+    elif event == cv.EVENT_LBUTTONUP:
+        drawing = False
+        if shape == 'r':
+            cv.rectangle(img, (x1, x2), (x, y), (0, 255, 0), -1)
+        if shape == 'l':
+            cv.line(img, (x1, x2), (x, y), (255, 255, 255), 3)
+        if shape == 'c':
+            cv.circle(img, (x, y), 10, (255, 255, 0), -1)
+img = cv.imread('lena.jpg')
+cv.namedWindow('image')
+cv.setMouseCallback('image', draw_circle)
+while(1):
+    cv.imshow('image', img)
+    key = cv.waitKey(1)
+    if key == ord('1'):
+        shape = 'r'
+    if key == ord('2'):
+        shape = 'l'
+    if key == ord('3'):
+        shape = 'c'
+#print (shape)
